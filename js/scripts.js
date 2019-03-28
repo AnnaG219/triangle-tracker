65ef1ab5530ca3504c745702d664cb0b2a422ec6
Anna Gibbons and Svitlana Filatova <anna@>
2019-03-28 15:25:59 -0700

    web page that does tracks triangles with if else statements

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  $("form#submit_info").submit(function(event) {
+    event.preventDefault();
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+    // if (age) {
+      if (side1 === side2 && side2 === side3 && side3 === side1) {
+        $('#notTriangle').hide();
+        $('#isosceles').hide();
+        $('#scalene').hide();
+        $('#equilateral').show();
+      }
+      else if (side1 === side2 || side2 === side3 || side3 === side1) {
+        $('#notTriangle').hide();
+        $('#scalene').hide();
+        $('#equilateral').hide();
+        $('#isosceles').show();
+      }
+      else if (!(side1+side2<=side3) || (side1+side3<=side2) || (side2+side3<=side1)) {
+      $('#notTriangle').hide();
+      $('#isosceles').hide();
+      $('#equilateral').hide();
+      $('#scalene').show();
+
+      }
+      else  {
+        $('#scalene').hide();
+        $('#isosceles').hide();
+        $('#equilateral').hide();
+        $('#notTriangle').show();
+      }
+  });
+});
